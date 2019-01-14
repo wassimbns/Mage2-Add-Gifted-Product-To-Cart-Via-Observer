a9530c2c4b7415a978ad0466ffc084e396b031ee
wassimbns <wassimsagonbensassi@gmail.com>
2019-01-14 18:08:26 +0100

    Update UpgradeSchema.php

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -19,59 +19,15 @@ class UpgradeSchema implements UpgradeSchemaInterface
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
-
-        if (version_compare($context->getVersion(), '2.0.4', '<')) {
-            $tableName = $setup->getTable('salesrule');
-            $columns = [
-                'popin_title' => [
-                    'type' => Table::TYPE_TEXT,
-                    'length' => 255,
-                    'nullable' => true,
-                    'comment' => 'Popin description text',
-                ],
-            ];
-            $connection = $setup->getConnection();
-            foreach ($columns as $name => $definition) {
-                $connection->addColumn($tableName, $name, $definition);
-            }
-        }
-
-        if (version_compare($context->getVersion(), '2.0.7', '<')) {
-
-            $setup->getConnection()->addColumn(
-                $setup->getTable('sales_order'),
-                'arriere',
-                [
-                    'type' => Table::TYPE_TEXT,
-                    'comment' => 'Arriérés des paiements'
-                ]
-            );
-
-            $setup->getConnection()->addColumn(
-                $setup->getTable('sales_order'),
-                'penalty',
-                [
-                    'type' => Table::TYPE_TEXT,
-                    'comment' => 'pinalités des paiements'
-                ]
-            );
-            $setup->getConnection()->addColumn(
-                $setup->getTable('sales_order'),
-                'ristourne',
-                [
-                    'type' => Table::TYPE_TEXT,
-                    'comment' => 'ristournes des paiements'
-                ]
-            );
-        }
-        if (version_compare($context->getVersion(), '2.0.11', '<')) {
+        
+        if (version_compare($context->getVersion(), '1.0.1', '<')) {
             $setup->getConnection()->addColumn(
                 $setup->getTable('quote_item'),
                 'is_gifted',
                 [
                     'type' => Table::TYPE_SMALLINT,
                     'default' => 0,
-                    'comment' => 'Cadeau ou Non'
+                    'comment' => 'Gifted or Not'
                 ]
             );
             $setup->getConnection()->addColumn(
@@ -79,7 +35,7 @@ class UpgradeSchema implements UpgradeSchemaInterface
                 'is_gifted',
                 [
                     'type' => Table::TYPE_SMALLINT,
-                    'comment' => 'Cadeau ou Non'
+                    'comment' => 'Gifted or Not'
                 ]
             );
         }
